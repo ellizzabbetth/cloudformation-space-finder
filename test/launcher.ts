f498988ed27367309a0b5bf8452bcf8eb46bc9e6
eli <ellizzabbetth@gmail.com>
2023-09-16 06:30:39 -0400

    here we go

---
+++ test/launcher.ts
@@ -0,0 +1,15 @@
+import { handler } from "../src/services/spaces/handler";
+
+process.env.AWS_REGION = "us-east-1";
+process.env.TABLE_NAME = "SpaceTable-0e36c8cd403d"
+
+
+handler({
+    httpMethod: 'GET',
+    queryStringParameters: {
+        id:'6eb6cab5-9612-4052-ba03-eba73ff7b739'
+    }
+    // body: JSON.stringify({
+    //     location: 'London'
+    // })
+} as any, {} as any);
