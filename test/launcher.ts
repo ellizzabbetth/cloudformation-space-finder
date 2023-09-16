0f63944ae6414627386541b37996b090fd34b4fe
eli <ellizzabbetth@gmail.com>
2023-09-16 15:54:21 -0400

    delet dynambodb

--- test/launcher.ts
+++ test/launcher.ts
@@ -5,11 +5,11 @@ process.env.TABLE_NAME = "SpaceTable-0e36c8cd403d"
 
 
 handler({
-    httpMethod: 'PUT',
+    httpMethod: 'DELETE',
     queryStringParameters: {
         id:'6eb6cab5-9612-4052-ba03-eba73ff7b739'
     },
-    body: JSON.stringify({
-        location: 'London updated'
-    })
+    // body: JSON.stringify({
+    //     location: 'London updated'
+    // })
 } as any, {} as any);
